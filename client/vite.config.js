f412037ecc97df683bc5fdfa3bab9e500ea68984
singharpt <104125829+singharpt@users.noreply.github.com>
2023-11-17 22:00:58 -0600

    changed vite config, removed build path

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,6 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "../server/public",
-    emptyOutDir: true,
-  },
   resolve: {
     alias: {
       picocss: path.resolve(__dirname, "../node_modules/@picocss/pico/css"),
